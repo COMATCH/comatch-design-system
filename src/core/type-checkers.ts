4bfae9c3be6743a8a8fe10a9ab1f3b6e203e0c8d
Stanimir Stoychev <xstukixs@gmail.com>
2020-10-14 09:42:22 +0200

    Prepare for release

--- src/core/type-checkers.ts
+++ src/core/type-checkers.ts
@@ -1,5 +1,6 @@
 import { isValidElement, ReactNode } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isValidReactNode(node: any): node is ReactNode {
     return isValidElement(node) || ['bigint', 'boolean', 'number', 'string'].includes(typeof node);
 }
